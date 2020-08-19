06347151ceab3f43dbcacbf2d9018b758f2b2450
HarryStevens <harryjosephstevens@gmail.com>
2020-08-18 21:04:11 -0400

    0.0.9

--- dist/d3-arrow.js
+++ dist/d3-arrow.js
@@ -1,4 +1,4 @@
-// https://github.com/HarryStevens/d3-arrow#readme Version 0.0.8. Copyright 2020 Harry Stevens.
+// https://github.com/HarryStevens/d3-arrow#readme Version 0.0.9. Copyright 2020 Harry Stevens.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
