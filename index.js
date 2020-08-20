b093208302edeadbbd69f00033934d0211c38ce5
HarryStevens <harryjosephstevens@gmail.com>
2020-08-19 22:17:54 -0400

    Export arrow3 from arrow-3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export {default as arrow1} from "./src/arrow-1";
 export {default as arrow2} from "./src/arrow-2";
-export {default as arrow3} from "./src/arrow-2";
+export {default as arrow3} from "./src/arrow-3";
 export {default as arrow5} from "./src/arrow-5";
 export {default as arrow10} from "./src/arrow-10";
 export {default as arrow11} from "./src/arrow-11";
